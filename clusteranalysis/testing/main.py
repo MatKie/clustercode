1b42bb16e9c550ba5a0ee9f0f33c5006c2314eb3
MatKie <m.kiesel18@imperial.ac.uk>
2019-07-12 16:37:36 +0100

    added some quick coded analysis to clustercode

--- clusteranalysis/testing/main.py
+++ clusteranalysis/testing/main.py
@@ -4,6 +4,8 @@ from clustering import cluster_analysis
 
 tpr  = "../files/nvt_2018.tpr"
 traj = "../files/nvt_short.xtc"
+traj  = "/home/mk8118/OneDrive/2019/simulations/gromacs/SDS/check_ensembles/NVT/PME_revised/nh_10/base/nvt.trr"
 
-print(cluster_analysis(tpr, ["CE", "CM"], traj))
+cluster_analysis(tpr, ["CE", "CM"], traj)
+
 
