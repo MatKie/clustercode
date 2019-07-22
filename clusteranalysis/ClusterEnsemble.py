52c7d4a9f8b60d2097214eec9f8efe23670b71f9
MatKie <m.kiesel18@imperial.ac.uk>
2019-07-22 16:21:52 +0100

    Added histogram plotting functionality

--- clusteranalysis/ClusterEnsemble.py
+++ clusteranalysis/ClusterEnsemble.py
@@ -1,6 +1,8 @@
 import MDAnalysis
 import MDAnalysis.lib.NeighborSearch as NeighborSearch
 import warnings
+import matplotlib as mlp
+import matplotlib.pyplot as plt
 #from MDAnalysis.core.groups import ResidueGroup
 """
 ToDo:
@@ -49,8 +51,8 @@ class ClusterEnsemble():
         self._traj  = traj # Protected Attribute
         self.cluster_objects = cluster_objects
 
-    def cluster_analysis(self, cut_off=7.5, style="atom", 
-                    measure="b2b", algorithm="dynamics"):
+    def cluster_analysis(self, cut_off=7.5, times=None, style="atom", 
+                    measure="b2b", algorithm="dynamic"):
         """High level function clustering molecules together
         
         Example
@@ -67,6 +69,10 @@ class ClusterEnsemble():
             Minimal distance for two particles to be in the
             same cluster, in Angstroem. Results still depend 
             on the measure parameter.
+        time : list of floats, optional
+            If None, do for whole trajectory. If an interval
+            is given like this (t_start, t_end) only do from start
+            to end.
         style : string, optional
             "atom" or "molecule". Dependent on this, the 
             cluster_objects attribute is interpreted as molecule
@@ -111,15 +117,21 @@ class ClusterEnsemble():
         else:
             raise NotImplementedError("{:s} is unspecified algorithm".format(
                                                                 algorithm))
-
+        
         # Loop over all trajectory times
         for time in self.universe.trajectory:
+            if times is not None:
+                if time.time > max(times) or  time.time < min(times):
+                    continue
             self.cluster_list.append(cluster_algorithm())
             print("****TIME: {:8.2f}".format(time.time))
             print("---->Number of clusters {:d}".format(
-                    len(self.cluster_list[-1]))
-                    )
-    
+                    len(self.cluster_list[-1])))
+
+        # Rewind Trajectory to beginning for other analysis
+        self.universe.trajectory.rewind()
+
+ 
     def _get_cluster_list_static(self, cut_off=7.5):
         """Get Cluster from single frame with the static method
         
@@ -187,7 +199,7 @@ class ClusterEnsemble():
                 merged_index.append(i)
                 # If the cluster is not completely new there will be a
                 # second intersection and we delete the previous cluster
-                # Note that len(merged_index) goes back to 1 afterwards
+                G# Note that len(merged_index) goes back to 1 afterwards
                 # and cluster_temp is the new merged cluster
                 if len(merged_index) > 1.5:
                     del cluster_list[merged_index[0]]
@@ -351,3 +363,91 @@ class ClusterEnsemble():
                         )
         
         return aggregate_species
+
+    def plot_histogram(self, ax, frames=(0, 1, 1), maxbins=False, 
+                       density=True, filename=None, *args, **kwargs):
+        """Method to plot histograms for different timeframes
+        
+        Examples
+        --------
+        None yet
+
+        Note
+        ----
+        By passing a axis to this function, loads of things can actually
+        be externally overriden to fit every need.
+
+        Parameters
+        ----------
+        ax : matplotlib axis object
+        frames : list of tuples, optional
+            for each tuple, the corresponding frames are averaged to
+            give one cluster size distribution. Give tuple as:
+            (first frame, last frame, stepsize to go throug frames)
+        maxbins : bool, optional
+            Set to true if you want as many bins as there are monomers
+            in the largest cluster
+        density : bool, optional
+            Whether or not to plot for absolute occurences or 
+            probabilities
+        filename : string, optional
+            If string is given, save the plot under that name. Specify
+            if you want pdf, png etc..
+
+        Returns
+        -------
+        ax : matplotlib axis object
+        """ 
+        # Check if the frames desired are available
+        if type(frames) is not list: frames = [frames] 
+        cluster_list_length = len(self.cluster_list)
+        maxframe = max([index[1] for index in frames])
+        if maxframe > cluster_list_length: 
+            raise ValueError("Upper Frame limit out of range, maximal frame \
+                              Number is {:d}".format(cluster_list_length))
+
+        # Get the size distribution of all frames for all frames
+        masterlist = []
+        for frames_i in frames:
+            masterlist.append(self._get_cluster_distribution(frames_i))
+
+        # By making as many bins as molecules in the largest cluster
+        # there is a bar for each clustersize
+        if maxbins == True:
+            bins = max([max(cluster_distribution) for cluster_distribution
+                                                  in masterlist]) 
+        else:
+            bins = None
+
+        ax.hist(masterlist, bins=bins, density=density,
+                 *args, **kwargs)
+        
+        ax.set_xlabel("Number of Monomers")
+        if not density == True : ystring = "Number of Occurrences"
+        else: ystring = "Probability"
+        ax.set_ylabel(ystring)
+     
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(filename)
+        
+    def _get_cluster_distribution(self, frames):
+        """Helper for plot_histogram to get a cluster distribution
+        
+        Parameters
+        ----------
+        frames : tuple of int
+            first frame, last frame and stepsize
+        
+        Returns
+        -------
+        cluster_distribution: list of int
+            All the clusterssizes in all the frames specified 
+        """ 
+        cluster_distribution = []
+        for frame in self.cluster_list[slice(*frames)]:
+            for cluster in frame:
+                cluster_distribution.append(len(cluster))
+        
+        return cluster_distribution
