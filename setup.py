0a6b65f4e327cb7705352d1e2178faa108620963
MatKie <m.kiesel18@imperial.ac.uk>
2020-04-01 17:20:12 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clustercode',
+    version='0.2.0',
+    url='https://tbd.com',
+    author='Matthias Kiesel, Tom Lindeboom',
+    author_email='m.kiesel18@imperial.ac.uk',
+    description='Modules designed to evaluate structural properties',
+    packages=find_packages(),    
+    install_requires=['numpy', 'mdanalysis => 0.19.2'],
+)
