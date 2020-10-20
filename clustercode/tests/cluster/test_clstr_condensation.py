3a3cb02fdd9870b7ac34e5b20a3e111cff45c8b1
MatKie <mtth.kiesel@gmail.com>
2020-10-20 19:58:11 +0100

    Uploadcommit

--- clustercode/tests/cluster/test_clstr_condensation.py
+++ clustercode/tests/cluster/test_clstr_condensation.py
@@ -10,10 +10,10 @@ mol = os.path.join("clustercode", "tests", "cluster", "files", "traj_mol.xtc")
 cluster = os.path.join("clustercode", "tests", "cluster", "files", "traj_cluster.xtc")
 whole = os.path.join("clustercode", "tests", "cluster", "files", "traj_whole.xtc")
 
-atom_uni = ClusterEnsemble(tpr, atom, ["C1" "C2", "C3", "C4"])
-mol_uni = ClusterEnsemble(tpr, mol, ["C1" "C2", "C3", "C4"])
-whole_uni = ClusterEnsemble(tpr, whole, ["C1" "C2", "C3", "C4"])
-clstr_uni = ClusterEnsemble(tpr, cluster, ["C1" "C2", "C3", "C4"])
+atom_uni = ClusterEnsemble(tpr, atom, ["C1", "C2", "C3", "C4"])
+mol_uni = ClusterEnsemble(tpr, mol, ["C1", "C2", "C3", "C4"])
+whole_uni = ClusterEnsemble(tpr, whole, ["C1", "C2", "C3", "C4"])
+clstr_uni = ClusterEnsemble(tpr, cluster, ["C1", "C2", "C3", "C4"])
 
 work_in = "Residue"  # atom, Residue
 measure = "b2b"  # b2b, COM, COG
@@ -21,6 +21,7 @@ pbc = True
 
 
 atom_uni.cluster_analysis(work_in=work_in, measure=measure, pbc=pbc)
+
 pbc = True
 method = "pkdtree"  # bruteforce, nsgrid, pkdtree
 ci = []
@@ -31,7 +32,6 @@ for clusters in atom_uni.cluster_list:
                 cluster, "SU", "NA", [4.4, 7.6], method=method, pbc=pbc
             )
         )
-
 
 tree = copy.deepcopy(ci)
 
@@ -69,23 +69,22 @@ mol_uni.cluster_analysis(work_in=work_in, measure=measure, pbc=pbc)
 whole_uni.cluster_analysis(work_in=work_in, measure=measure, pbc=pbc)
 clstr_uni.cluster_analysis(work_in=work_in, measure=measure, pbc=pbc)
 
-method = "pkdtree"  # bruteforce, nsgrid, pkdtree
 mol, whole, cluster = [], [], []
-for m, w, c in zip(
-    mol_uni.cluster_list, whole_uni.cluster_list, clstr_uni.cluster_list
-):
-    for mi, wi, ci in zip(m, w, c):
-        mol.append(
-            mol_uni.condensed_ions(mi, "SU", "NA", [4.4, 7.6], method=method, pbc=pbc)
-        )
-        whole.append(
-            whole_uni.condensed_ions(wi, "SU", "NA", [4.4, 7.6], method=method, pbc=pbc)
-        )
-        cluster.append(
-            clstr_uni.condensed_ions(ci, "SU", "NA", [4.4, 7.6], method=method, pbc=pbc)
-        )
+def condens(uni, this_list, pbc):
+    pbc=pbc
+    method = "pkdtree"  # bruteforce, nsgrid, pkdtree
+    for clusters in uni.cluster_list:
+        for cluster in clusters:
+            this_list.append(
+                uni.condensed_ions(cluster, 'SU', 'NA', [4.4, 7.6], method=method, pbc=pbc)
+                )
 
-for ti, gi, bi, ci in zip(tree, mol, whole, cluster):
+condens(mol_uni, mol, pbc)
+condens(whole_uni, whole, pbc)
+condens(clstr_uni, cluster, pbc)
+
+
+for i, (ti, gi, bi, ci) in enumerate(zip(tree, mol, whole, cluster)):
     for tii, gii, bii, cii in zip(ti, gi, bi, ci):
         try:
             assert tii == approx(gii)
@@ -95,6 +94,7 @@ for ti, gi, bi, ci in zip(tree, mol, whole, cluster):
             assert gii == approx(cii)
             assert bii == approx(cii)
         except:
+            print(i)
             print(ti, gi, bi, ci)
 
 
@@ -113,15 +113,16 @@ mol_list = get_lengths(mol_uni)
 whole_list = get_lengths(whole_uni)
 clstr_list = get_lengths(clstr_uni)
 
+
 for a, m, w, c in zip(atom_list, mol_list, whole_list, clstr_list):
     for ai, mi, wi, ci in zip(a, m, w, c):
         if ai != mi or ai != wi or ai != ci:
+            print('Different clustering of atom')
             print(ai, mi, wi, ci)
-            raise RuntimeError("Different clustering of atom")
         if mi != wi or mi != ci:
+            print("Different clustering of mol")
             print(ai, mi, wi, ci)
-            raise RuntimeError("Different clustering of mol")
         if wi != ci:
+            print("Different clustering of clstr")
             print(ai, mi, wi, ci)
-            raise RuntimeError("Different clustering of whole")
 
