e0f0cdd821ad30b3783b4d1dc5265caaa70188ef
MatKie <mtth.kiesel@gmail.com>
2022-03-06 22:43:54 +0000

    Forgot to actually commit test_gyr two commits ago..

--- clustercode/tests/cluster/test_gyr.py
+++ clustercode/tests/cluster/test_gyr.py
@@ -12,6 +12,7 @@ clstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4"])
 
 clstr.cluster_analysis()
 
+# gmx gyrate -f traj_pbc_problematic_cluster.xtc -s topol_no_solv.tpr -p yes
 gmx_gyrate = [
     [3.28795, 1.86045, 2.97682, 3.04934],
     [3.22206, 1.86464, 2.9183, 2.96141],
@@ -19,6 +20,14 @@ gmx_gyrate = [
     [3.17957, 1.90879, 2.83768, 2.91949],
     [3.17345, 1.86303, 2.87302, 2.90112],
     [3.19677, 1.83934, 2.91293, 2.92753],
+]
+gmx_principal = [
+    [3.50891250, 8.983379375, 9.426435000],
+    [3.52470625, 8.633655000, 8.890634375],
+    [3.65722625, 8.453534375, 8.707842500],
+    [3.69359125, 8.163197500, 8.640699375],
+    [3.51862312, 8.367801875, 8.532266875],
+    [3.42970875, 8.601878750, 8.688352500],
 ]
 
 
@@ -37,3 +46,19 @@ class TestGyration:
                 for item, other_item in zip(their_gyrate, our_gyrate):
                     assert item == approx(other_item, abs=5e-4)
 
+    def test_moment_of_inertia(self):
+        """
+        This test checks if the result for the inertia tensor is the same
+        than in gromacs. There is a relatively high deviation of up to 
+        0.13 units. 
+        % WHAT ARE THE UNITS? WHY DO WE DIVIDE BY THE MASS IN .inertia_tensor()?
+        """
+        for clusters, their_inertia in zip(clstr.cluster_list, gmx_principal):
+            for cluster in clusters:
+                clstr.unwrap_cluster(cluster)
+                our_inertia = clstr.inertia_tensor(cluster)
+                for item, other_item in zip(our_inertia, their_inertia):
+                    assert item == approx(other_item, abs=0.13)
+
+
+# TestGyration().test_moment_of_inertia()
