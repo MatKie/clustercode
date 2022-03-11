793a232d7f70dc6ba489e1d40b6434a4ef32df77
MatKie <mtth.kiesel@gmail.com>
2022-03-11 15:16:05 +0000

    Add more clustering tests

--- clustercode/tests/cluster/test_clustering.py
+++ clustercode/tests/cluster/test_clustering.py
@@ -14,9 +14,20 @@ MainClstr.cluster_analysis(
 
 main_cluster_sizes = MainClstr.cluster_sizes
 
+MolClstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4", "SU"])
+MolClstr.cluster_analysis(
+    cut_off=6.5, style="atom", measure="b2b", algorithm="dynamic", work_in="Residue"
+)
+
+mol_cluster_sizes = MolClstr.cluster_sizes
+
 
 class TestClustering:
     def test_algorithm_static(self):
+        """
+        Check if static and dynamic keywords get the same cluster
+        size distribution
+        """
         SideClstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4"])
         SideClstr.cluster_analysis(
             cut_off=6.5,
@@ -29,3 +40,102 @@ class TestClustering:
             assert len(main_sizes) == approx(len(side_sizes))
             for main_cluster, side_cluster in zip(main_sizes, side_sizes):
                 assert main_cluster == approx(side_cluster)
+
+    def test_work_in_atom(self):
+        """
+        Check if work in atom and work in residue keywords get the same cluster
+        size distribution. Got to divide by number of atoms for this
+        """
+        SideClstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4"])
+        SideClstr.cluster_analysis(
+            cut_off=6.5,
+            style="atom",
+            measure="b2b",
+            algorithm="static",
+            work_in="Atom",
+        )
+        for main_sizes, side_sizes in zip(main_cluster_sizes, SideClstr.cluster_sizes):
+            assert len(main_sizes) == approx(len(side_sizes))
+            for main_cluster, side_cluster in zip(main_sizes, side_sizes):
+                assert main_cluster == approx(side_cluster / 4)
+
+    def test_measure_COM(self):
+        """
+        Check if measur distance by COM does result in the same cluster
+        size distribution. Not really a good test as it physically
+        does not need to result in the same cluster size distribution.
+        Apparently it does for this trajectory tho.
+        """
+        SideClstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4"])
+        SideClstr.cluster_analysis(
+            cut_off=6.5,
+            style="atom",
+            measure="COM",
+            algorithm="dynamic",
+            work_in="Residue",
+        )
+        for main_sizes, side_sizes in zip(main_cluster_sizes, SideClstr.cluster_sizes):
+            assert len(main_sizes) == approx(len(side_sizes))
+            for main_cluster, side_cluster in zip(main_sizes, side_sizes):
+                assert main_cluster == approx(side_cluster)
+
+    def test_measure_COG(self):
+        """
+        Check if measur distance by COG does result in the same cluster
+        size distribution. Not really a good test as it physically
+        does not need to result in the same cluster size distribution.
+        Apparently it does for this trajectory tho.
+        """
+        SideClstr = ClusterEnsemble(tpr, traj, ["C1", "C2", "C3", "C4"])
+        SideClstr.cluster_analysis(
+            cut_off=6.5,
+            style="atom",
+            measure="COG",
+            algorithm="dynamic",
+            work_in="Residue",
+        )
+        for main_sizes, side_sizes in zip(main_cluster_sizes, SideClstr.cluster_sizes):
+            assert len(main_sizes) == approx(len(side_sizes))
+            for main_cluster, side_cluster in zip(main_sizes, side_sizes):
+                assert main_cluster == approx(side_cluster)
+
+    def test_style_molecule(self):
+        """
+        Check if style molecule gets the same result than style atom.
+        Need to use different reference, which includes headgroups
+        for style atom, as it actually makes a difference for the neighbour
+        search.
+        """
+        SideClstr = ClusterEnsemble(tpr, traj, ["SDS"])
+        SideClstr.cluster_analysis(
+            cut_off=6.5,
+            style="molecule",
+            measure="b2b",
+            algorithm="dynamic",
+            work_in="Residue",
+        )
+        for main_sizes, side_sizes in zip(mol_cluster_sizes, SideClstr.cluster_sizes):
+            assert len(main_sizes) == approx(len(side_sizes))
+            for main_cluster, side_cluster in zip(main_sizes, side_sizes):
+                assert main_cluster == approx(side_cluster)
+
+    def test_style_molecule_work_in_Atom(self):
+        """
+        Check if style molecule gets the same result than style atom
+        if used with work in atom keyword.
+        Need to use different reference, which includes headgroups
+        for style atom, as it actually makes a difference for the neighbour
+        search.
+        """
+        SideClstr = ClusterEnsemble(tpr, traj, ["SDS"])
+        SideClstr.cluster_analysis(
+            cut_off=6.5,
+            style="molecule",
+            measure="b2b",
+            algorithm="dynamic",
+            work_in="Atom",
+        )
+        for main_sizes, side_sizes in zip(mol_cluster_sizes, SideClstr.cluster_sizes):
+            assert len(main_sizes) == approx(len(side_sizes))
+            for main_cluster, side_cluster in zip(main_sizes, side_sizes):
+                assert main_cluster == approx(side_cluster / 5)
