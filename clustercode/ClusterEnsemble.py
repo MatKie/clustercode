e8618641ee46207e6afe57930a2572ec055b9596
MatKie <mtth.kiesel@gmail.com>
2022-03-15 15:05:53 +0000

    Refactor ClusterEnsemble.py. Copy code into regions

--- clustercode/ClusterEnsemble.py
+++ clustercode/ClusterEnsemble.py
@@ -195,6 +195,212 @@ class ClusterEnsemble(BaseUniverse):
         self.universe.trajectory.rewind()
         self.cluster_list = self._create_generator(self.cluster_list)
 
+    def _get_cluster_list_static(self, cut_off=7.5):
+        """Get Cluster from single frame with the static method
+        
+        This code simply loops over all atoms in the aggregate
+        species and finds a cluster for each atom (all neighbours).
+        This cluster is merged into the already found clusters.
+
+        Parameters
+        ----------
+        cut_off : float, optional
+            Radius around which to search for neighbours 
+
+        Returns
+        -------
+        cluster_list : list of ResGroups or AtomGroups
+        """
+        cluster_list = []
+        if self.search_level == "R":
+            aggregate_species_atoms = self.aggregate_species.groupby("resids").values()
+        elif self.search_level == "A":
+            aggregate_species_atoms = self.aggregate_species
+
+        for atoms in aggregate_species_atoms:
+            cluster_temp = set(
+                self.neighbour_search.search(
+                    atoms=atoms, radius=cut_off, level=self.search_level
+                )
+            )
+
+            cluster_list = self._merge_cluster(cluster_list, cluster_temp)
+
+        return cluster_list
+
+    def _merge_cluster(self, cluster_list, cluster_temp):
+        """Code to merge a cluster into a cluster list
+        
+        This code merges a cluster into a clusterlist by reverse
+        looping over the cluster list. Whenever an overlap is detected
+        the new, temporary, cluster is merged into the old one and
+        the new one is deleted. At the same time the temporary cluster
+        is updated to be the old one merged with the new one to
+        further look for overlap.
+
+        Parameters
+        ----------
+        cluster_list : list of sets of clusterids
+            All the sets of clusters already present.
+        cluster_temp : set of clusterids
+            Last found cluster, to be merged into the cluster_list
+
+        Returns
+        -------
+        cluster_list : list of sets of clusterids
+            Updated cluster_list
+
+        """
+        cluster_list.append(cluster_temp)
+        merged_index = []
+
+        # Loop through cluster_list in reverse with the actual also
+        # the revesed indices
+        for i, cluster in reversed(list(enumerate(cluster_list))):
+            # If there is an intersection start merging. There is
+            # always an intersection with the first cluster (identical)
+            if bool(cluster.intersection(cluster_temp)):
+                cluster_temp = cluster_temp | cluster  # Updating cluster_temp
+                cluster_list[i] = cluster_temp
+                merged_index.append(i)
+                # If the cluster is not completely new there will be a
+                # second intersection and we delete the previous cluster
+                # Note that len(merged_index) goes back to 1 afterwards
+                # and cluster_temp is the new merged cluster
+                if len(merged_index) > 1.5:
+                    del cluster_list[merged_index[0]]
+                    del merged_index[0]
+                elif len(merged_index) > 1.5:
+                    warnings.warn("Wrong behaviour in cluster merge", UserWarning)
+
+        return cluster_list
+
+    def _get_cluster_list_dynamic(self, cut_off=7.5):
+        """Get Cluster from single frame with dynamic algorithm
+
+        Faster algorithm to find clusters. We single out one
+        molecule or atom and find all its neighbours, then we call the same
+        function again and find the neighbours neighbours,
+        while excluding already found neighbours. We do this until
+        we cant find any more neighbours. Then we choose another
+        molecule or atom until we do not have any molecules/atoms left.
+
+        Parameters
+        ----------
+        cut_off : float, optional
+            Radius around which to search for neighbours
+
+        Returns
+        -------
+        cluster_list : list of ResGroups or AtomGroups
+
+        """
+        cluster_list = []
+
+        if self.search_level == "R":
+            aggregate_species = self.aggregate_species.residues
+        elif self.search_level == "A":
+            aggregate_species = self.aggregate_species
+
+        # Loop until all molecules have been in clusters
+        while len(aggregate_species) > 0:
+            # Initialize the search_set and temporary cluster with
+            # one molecule or atom
+            if self.search_level == "R":
+                species_temp = aggregate_species[0].atoms.residues
+            elif self.search_level == "A":
+                species_temp = MDAnalysis.core.groups.AtomGroup([aggregate_species[0]])
+            search_set = species_temp
+            cluster_temp = species_temp
+            # In this loop search set gets updated to only have new
+            # neighours and cluster_temp grows
+            while len(search_set) > 0:
+                search_set, cluster_temp = self._grow_cluster(
+                    cut_off, search_set, cluster_temp
+                )
+            # Once no more neighbours are found add the cluster to
+            # the list and subtract the cluster from the aggregate
+            # species.
+            # Possible Improvement: Update NeighbourSearch object
+            cluster_list.append(cluster_temp)
+            aggregate_species = aggregate_species.difference(cluster_temp)
+
+        return cluster_list
+
+    def _grow_cluster(self, cut_off, search_set, cluster_temp):
+        """Code to grow a cluster (cluster_temp) and obtain new search set
+        (search_set)
+
+        This algorithm looks for neighbours of atoms in search_set
+        and adds them to the temporary cluster. The search_set
+        is updated to only include newly found neighbours not
+        already present in search_set. The neighbours searching
+        still looks in all the atoms present in the aggregate_species.
+
+        Parameters
+        ----------
+        cut_off : float, optional
+            Radius around which to search for neighbours
+        search_set : MDAnalysis ResidueGroup
+            Atoms for which to look for neighbours
+        cluster_temp : MDAanalysis ResidueGroup
+            All the atoms/residues currently in the cluster
+
+        Returns
+        ------- 
+        search_set : MDAnalysis ResidueGroup
+            Atoms for which to look for neighbours updated to be
+            the latest found ones
+        cluster_temp : MDAanalysis ResidueGroup
+            All the atoms/residues currently in the cluster updated
+            to include the latest found ones
+
+        """
+
+        if self.search_level == "R":
+            # Find neighbours and cast into ResidueGroup
+            new_cluster_species = MDAnalysis.core.groups.ResidueGroup(
+                self.neighbour_search.search(
+                    atoms=self._select_species(search_set.atoms, style=self.style),
+                    radius=cut_off,
+                    level=self.search_level,
+                )
+            )
+        elif self.search_level == "A":
+            new_cluster_species = self.neighbour_search.search(
+                atoms=search_set.atoms, radius=cut_off, level=self.search_level
+            )
+
+        # The new search_set should only have atoms not already in the cluster
+        search_set = new_cluster_species.difference(cluster_temp)
+        # The new temporary cluster is updated
+        cluster_temp = cluster_temp.union(new_cluster_species)
+
+        return search_set, cluster_temp
+
+    def _create_generator(self, cluster_list):
+        """
+        Make cluster_list a generator expression.
+
+        This works a bit weirdly. when looping over 
+        it, all transformations are only valid as long
+        as the trajectory is not loaded again. For example
+        when you run the loop do the cluster unwrapping
+        and then run the loop again, the unwrapping will
+        be 'lost'.
+        """
+        i = 0
+        for j, time in enumerate(self.universe.trajectory):
+            if self.times is not None:
+                if time.time < min(self.times):
+                    i = i + 1
+                    continue
+                elif time.time > max(self.times):
+                    break
+            yield cluster_list[j - i]
+
+        self.cluster_list = self._create_generator(cluster_list)
+
     def condensed_ions(
         self,
         cluster,
@@ -808,212 +1014,6 @@ class ClusterEnsemble(BaseUniverse):
             r1_norm = np.linalg.norm(a - b)
             distances.append(r1_norm)
         return distances
-
-    def _create_generator(self, cluster_list):
-        """
-        Make cluster_list a generator expression.
-
-        This works a bit weirdly. when looping over 
-        it, all transformations are only valid as long
-        as the trajectory is not loaded again. For example
-        when you run the loop do the cluster unwrapping
-        and then run the loop again, the unwrapping will
-        be 'lost'.
-        """
-        i = 0
-        for j, time in enumerate(self.universe.trajectory):
-            if self.times is not None:
-                if time.time < min(self.times):
-                    i = i + 1
-                    continue
-                elif time.time > max(self.times):
-                    break
-            yield cluster_list[j - i]
-
-        self.cluster_list = self._create_generator(cluster_list)
-
-    def _get_cluster_list_static(self, cut_off=7.5):
-        """Get Cluster from single frame with the static method
-        
-        This code simply loops over all atoms in the aggregate
-        species and finds a cluster for each atom (all neighbours).
-        This cluster is merged into the already found clusters.
-
-        Parameters
-        ----------
-        cut_off : float, optional
-            Radius around which to search for neighbours 
-
-        Returns
-        -------
-        cluster_list : list of ResGroups or AtomGroups
-        """
-        cluster_list = []
-        if self.search_level == "R":
-            aggregate_species_atoms = self.aggregate_species.groupby("resids").values()
-        elif self.search_level == "A":
-            aggregate_species_atoms = self.aggregate_species
-
-        for atoms in aggregate_species_atoms:
-            cluster_temp = set(
-                self.neighbour_search.search(
-                    atoms=atoms, radius=cut_off, level=self.search_level
-                )
-            )
-
-            cluster_list = self._merge_cluster(cluster_list, cluster_temp)
-
-        return cluster_list
-
-    def _merge_cluster(self, cluster_list, cluster_temp):
-        """Code to merge a cluster into a cluster list
-        
-        This code merges a cluster into a clusterlist by reverse
-        looping over the cluster list. Whenever an overlap is detected
-        the new, temporary, cluster is merged into the old one and
-        the new one is deleted. At the same time the temporary cluster
-        is updated to be the old one merged with the new one to
-        further look for overlap.
-
-        Parameters
-        ----------
-        cluster_list : list of sets of clusterids
-            All the sets of clusters already present.
-        cluster_temp : set of clusterids
-            Last found cluster, to be merged into the cluster_list
-
-        Returns
-        -------
-        cluster_list : list of sets of clusterids
-            Updated cluster_list
-
-        """
-        cluster_list.append(cluster_temp)
-        merged_index = []
-
-        # Loop through cluster_list in reverse with the actual also
-        # the revesed indices
-        for i, cluster in reversed(list(enumerate(cluster_list))):
-            # If there is an intersection start merging. There is
-            # always an intersection with the first cluster (identical)
-            if bool(cluster.intersection(cluster_temp)):
-                cluster_temp = cluster_temp | cluster  # Updating cluster_temp
-                cluster_list[i] = cluster_temp
-                merged_index.append(i)
-                # If the cluster is not completely new there will be a
-                # second intersection and we delete the previous cluster
-                # Note that len(merged_index) goes back to 1 afterwards
-                # and cluster_temp is the new merged cluster
-                if len(merged_index) > 1.5:
-                    del cluster_list[merged_index[0]]
-                    del merged_index[0]
-                elif len(merged_index) > 1.5:
-                    warnings.warn("Wrong behaviour in cluster merge", UserWarning)
-
-        return cluster_list
-
-    def _get_cluster_list_dynamic(self, cut_off=7.5):
-        """Get Cluster from single frame with dynamic algorithm
-
-        Faster algorithm to find clusters. We single out one
-        molecule or atom and find all its neighbours, then we call the same
-        function again and find the neighbours neighbours,
-        while excluding already found neighbours. We do this until
-        we cant find any more neighbours. Then we choose another
-        molecule or atom until we do not have any molecules/atoms left.
-
-        Parameters
-        ----------
-        cut_off : float, optional
-            Radius around which to search for neighbours
-
-        Returns
-        -------
-        cluster_list : list of ResGroups or AtomGroups
-
-        """
-        cluster_list = []
-
-        if self.search_level == "R":
-            aggregate_species = self.aggregate_species.residues
-        elif self.search_level == "A":
-            aggregate_species = self.aggregate_species
-
-        # Loop until all molecules have been in clusters
-        while len(aggregate_species) > 0:
-            # Initialize the search_set and temporary cluster with
-            # one molecule or atom
-            if self.search_level == "R":
-                species_temp = aggregate_species[0].atoms.residues
-            elif self.search_level == "A":
-                species_temp = MDAnalysis.core.groups.AtomGroup([aggregate_species[0]])
-            search_set = species_temp
-            cluster_temp = species_temp
-            # In this loop search set gets updated to only have new
-            # neighours and cluster_temp grows
-            while len(search_set) > 0:
-                search_set, cluster_temp = self._grow_cluster(
-                    cut_off, search_set, cluster_temp
-                )
-            # Once no more neighbours are found add the cluster to
-            # the list and subtract the cluster from the aggregate
-            # species.
-            # Possible Improvement: Update NeighbourSearch object
-            cluster_list.append(cluster_temp)
-            aggregate_species = aggregate_species.difference(cluster_temp)
-
-        return cluster_list
-
-    def _grow_cluster(self, cut_off, search_set, cluster_temp):
-        """Code to grow a cluster (cluster_temp) and obtain new search set
-        (search_set)
-
-        This algorithm looks for neighbours of atoms in search_set
-        and adds them to the temporary cluster. The search_set
-        is updated to only include newly found neighbours not
-        already present in search_set. The neighbours searching
-        still looks in all the atoms present in the aggregate_species.
-
-        Parameters
-        ----------
-        cut_off : float, optional
-            Radius around which to search for neighbours
-        search_set : MDAnalysis ResidueGroup
-            Atoms for which to look for neighbours
-        cluster_temp : MDAanalysis ResidueGroup
-            All the atoms/residues currently in the cluster
-
-        Returns
-        ------- 
-        search_set : MDAnalysis ResidueGroup
-            Atoms for which to look for neighbours updated to be
-            the latest found ones
-        cluster_temp : MDAanalysis ResidueGroup
-            All the atoms/residues currently in the cluster updated
-            to include the latest found ones
-
-        """
-
-        if self.search_level == "R":
-            # Find neighbours and cast into ResidueGroup
-            new_cluster_species = MDAnalysis.core.groups.ResidueGroup(
-                self.neighbour_search.search(
-                    atoms=self._select_species(search_set.atoms, style=self.style),
-                    radius=cut_off,
-                    level=self.search_level,
-                )
-            )
-        elif self.search_level == "A":
-            new_cluster_species = self.neighbour_search.search(
-                atoms=search_set.atoms, radius=cut_off, level=self.search_level
-            )
-
-        # The new search_set should only have atoms not already in the cluster
-        search_set = new_cluster_species.difference(cluster_temp)
-        # The new temporary cluster is updated
-        cluster_temp = cluster_temp.union(new_cluster_species)
-
-        return search_set, cluster_temp
 
     def plot_histogram(
         self,
